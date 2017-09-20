ac4bbead57cf276753394b6eb8fc2c5388d347ed
Scott Parlane <scott.parlane@alliedtelesis.co.nz>
2017-09-20 12:57:31 +1200

    package: Correct the bd:restore path
    /package/ was being lost out of the bd:restore path,
    Also files from overlays weren't always being picked up.
    
    Change-Id: Ic267d66855c12524f3645db0c63b80d0e2f4e521
    Reviewed-by: Ridge Kennedy <ridge.kennedy@alliedtelesis.co.nz>
    Reviewed-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>
    Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

--- package.cpp
+++ package.cpp
@@ -60,10 +60,12 @@ char *Package::absolute_fetch_path(const char *location)
 {
 	char *src_path = NULL;
 	const char *cwd = WORLD->getWorkingDir()->c_str();
-	if(location[0] == '/' || !strncmp(location, "dl/", 3) || location[0] == '.') {
+	if(location[0] == '/' || !strncmp(location, "dl/", 3)) {
 		asprintf(&src_path, "%s/%s", cwd, location);
+	} else if(location[0] == '.') {
+		asprintf(&src_path, "%s/%s/%s", cwd, this->getOverlay().c_str(), location);
 	} else {
-		asprintf(&src_path, "%s/%s/%s/%s", cwd, this->getOverlay().c_str(),
+		asprintf(&src_path, "%s/%s/package/%s/%s", cwd, this->getOverlay().c_str(),
 			 this->getName().c_str(), location);
 	}
 	return src_path;
@@ -72,8 +74,10 @@ char *Package::absolute_fetch_path(const char *location)
 char *Package::relative_fetch_path(const char *location)
 {
 	char *src_path = NULL;
-	if(location[0] == '/' || !strncmp(location, "dl/", 3) || location[0] == '.') {
+	if(location[0] == '/' || !strncmp(location, "dl/", 3)) {
 		src_path = strdup(location);
+	} else if(location[0] == '.') {
+		asprintf(&src_path, "%s/%s", this->getOverlay().c_str(), location);
 	} else {
 		asprintf(&src_path, "%s/package/%s/%s", this->getOverlay().c_str(),
 			 this->getName().c_str(), location);
