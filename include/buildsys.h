18ba48e360537eb7ab5cf031cab90c25af2cf455
Matt Bennett <matt.bennett@alliedtelesis.co.nz>
2019-07-19 09:23:20 +1200

    Add string_format function
    This will allow for the C style asprintf usages to be replaced with a
    more C++ style of using strings.
    
    Change-Id: I55b7f41aa70d270ca0cdfde8efbf09cc6b239ee4
    Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
    Reviewed-by: Ridge Kennedy <ridge.kennedy@alliedtelesis.co.nz>
    Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

--- include/buildsys.h
+++ include/buildsys.h
@@ -1642,6 +1642,21 @@ namespace buildsys {
 	void hash_setup(void);
 	char *hash_file(const char *fname);
 	void hash_shutdown(void);
+
+	/** @brief A C++ style replacement for C style sprintf/asprintf. Returns a formatted
+	 *         string based on the input format and arguments.
+	 *
+	 * @param format The format to print with.
+	 * @param args (variadic) The arguments to use with the format.
+	 * @return The formatted string.
+	 */
+	template < typename ... Args >
+	    std::string string_format(const std::string & format, Args ... args) {
+		size_t size = snprintf(nullptr, 0, format.c_str(), args ...) + 1;	// Extra space for '\0'
+		std::unique_ptr < char[] > buf(new char[size]);
+		snprintf(buf.get(), size, format.c_str(), args ...);
+		return std::string(buf.get(), buf.get() + size - 1);	// We don't want the '\0' inside
+	}
 };
 
 using namespace buildsys;
