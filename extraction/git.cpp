alliedtelesis/buildsyspp

commit 3a4313da70eda61f6d6a03b4adbda4adefc434e1

Enable -Wshadow compiler flag
Shadowing member variable names can introduce subtle bugs if the
developer forgets scoping rules and/or the scoping changes. Rename
variables to avoid shadowing.

Change-Id: Ia933016b9590d977095fc32be7214e0cc642669d
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

extraction/git.cpp
@@ -131,13 +131,13 @@ std::string GitDirExtractionUnit::dirtyHash()
 	return git_diff_hash(this->localPath());
 }
 
-GitExtractionUnit::GitExtractionUnit(const std::string &remote, const std::string &local,
-                                     std::string refspec, Package *P)
+GitExtractionUnit::GitExtractionUnit(const std::string &remote, const std::string &_local,
+                                     std::string _refspec, Package *_P)
 {
 	this->uri = remote;
-	this->local = P->getWorld()->getWorkingDir() + "/source/" + local;
-	this->refspec = refspec;
-	this->P = P;
+	this->local = P->getWorld()->getWorkingDir() + "/source/" + _local;
+	this->refspec = _refspec;
+	this->P = _P;
 	this->fetched = false;
 }
 
@@ -233,16 +233,16 @@ bool GitExtractionUnit::fetch(BuildDir *d)
 	}
 	bool res = true;
 
-	std::string hash = git_hash(source_dir);
+	std::string _hash = git_hash(source_dir);
 
 	if(!this->hash.empty()) {
-		if(this->hash != hash) {
+		if(this->hash != _hash) {
 			log(this->P, "Hash mismatch for %s\n(committed to %s, providing %s)\n",
-			    this->uri.c_str(), this->hash.c_str(), hash.c_str());
+			    this->uri.c_str(), this->hash.c_str(), _hash.c_str());
 			res = false;
 		}
 	} else {
-		this->hash = hash;
+		this->hash = _hash;
 	}
 
 	this->fetched = res;
@@ -268,20 +268,20 @@ std::string GitExtractionUnit::HASH()
 	return this->hash;
 }
 
-bool GitExtractionUnit::extract(Package *P)
+bool GitExtractionUnit::extract(Package *_P)
 {
 	// make sure it has been fetched
 	if(!this->fetched) {
-		if(!this->fetch(P->builddir())) {
+		if(!this->fetch(_P->builddir())) {
 			return false;
 		}
 	}
 	// copy to work dir
-	PackageCmd pc(P->builddir()->getPath(), "cp");
+	PackageCmd pc(_P->builddir()->getPath(), "cp");
 	pc.addArg("-dpRuf");
 	pc.addArg(this->localPath());
 	pc.addArg(".");
-	if(!pc.Run(P))
+	if(!pc.Run(_P))
 		throw CustomException("Failed to checkout");
 
 	return true;

Makefile
@@ -8,7 +8,7 @@ LUAVERSION := $(shell pkg-config --exists lua && echo lua || (pkg-config --exist
 ifeq ($(LUAVERSION),none)
 $(error Can't find lua, please install and/or check that pkg-config knows about it)
 endif
-WARNFLAGS	:= -Werror -Wall -Wextra
+WARNFLAGS	:= -Werror -Wall -Wextra -Wshadow
 BASEFLAGS	:= $(WARNFLAGS) -ggdb2 -pthread $(shell pkg-config --cflags $(LUAVERSION)) $(USER_DEFINES)
 CXXFLAGS	:= -std=c++11 $(BASEFLAGS)
 CFLAGS		:= -std=c99 $(BASEFLAGS)

buildinfo.cpp
@@ -30,15 +30,15 @@ void BuildDescription::add(BuildUnit *bu)
 	this->BUs.push_back(bu);
 }
 
-PackageFileUnit::PackageFileUnit(const std::string &fname, const std::string &fname_short)
+PackageFileUnit::PackageFileUnit(const std::string &fname, const std::string &_fname_short)
 {
-	this->uri = fname_short;
+	this->uri = _fname_short;
 	this->hash = hash_file(fname);
 }
 
-RequireFileUnit::RequireFileUnit(const std::string &fname, const std::string &fname_short)
+RequireFileUnit::RequireFileUnit(const std::string &fname, const std::string &_fname_short)
 {
-	this->uri = fname_short;
+	this->uri = _fname_short;
 	this->hash = hash_file(fname);
 }
 
@@ -48,10 +48,10 @@ ExtractionInfoFileUnit::ExtractionInfoFileUnit(const std::string &fname)
 	this->hash = hash_file(fname + ".new");
 }
 
-BuildInfoFileUnit::BuildInfoFileUnit(const std::string &fname, const std::string &hash)
+BuildInfoFileUnit::BuildInfoFileUnit(const std::string &fname, const std::string &_hash)
 {
 	this->uri = fname;
-	this->hash = hash;
+	this->hash = _hash;
 }
 
 OutputInfoFileUnit::OutputInfoFileUnit(const std::string &fname)

extraction.cpp
@@ -142,15 +142,15 @@ bool ZipExtractionUnit::extract(Package *P)
 	return true;
 }
 
-PatchExtractionUnit::PatchExtractionUnit(int level, const std::string &patch_path,
+PatchExtractionUnit::PatchExtractionUnit(int _level, const std::string &_patch_path,
                                          const std::string &patch_fname,
-                                         const std::string &fname_short)
+                                         const std::string &_fname_short)
 {
 	this->uri = patch_fname;
-	this->fname_short = fname_short;
+	this->fname_short = _fname_short;
 	this->hash = hash_file(this->uri);
-	this->level = level;
-	this->patch_path = patch_path;
+	this->level = _level;
+	this->patch_path = _patch_path;
 }
 
 bool PatchExtractionUnit::extract(Package *P)
@@ -185,10 +185,10 @@ bool PatchExtractionUnit::extract(Package *P)
 }
 
 FileCopyExtractionUnit::FileCopyExtractionUnit(const std::string &fname,
-                                               const std::string &fname_short)
+                                               const std::string &_fname_short)
 {
 	this->uri = fname;
-	this->fname_short = fname_short;
+	this->fname_short = _fname_short;
 	this->hash = hash_file(this->uri);
 }
 
@@ -214,12 +214,12 @@ bool FileCopyExtractionUnit::extract(Package *P)
 	return true;
 }
 
-FetchedFileCopyExtractionUnit::FetchedFileCopyExtractionUnit(FetchUnit *fetched,
-                                                             const std::string &fname_short)
+FetchedFileCopyExtractionUnit::FetchedFileCopyExtractionUnit(
+    FetchUnit *_fetched, const std::string &_fname_short)
 {
-	this->fetched = fetched;
-	this->uri = fetched->relative_path();
-	this->fname_short = fname_short;
+	this->fetched = _fetched;
+	this->uri = _fetched->relative_path();
+	this->fname_short = _fname_short;
 	this->hash = std::string("");
 }
 

fetch.cpp
@@ -45,13 +45,13 @@ std::string DownloadFetch::full_name()
 /* This is the final name, without any compressed extension */
 std::string DownloadFetch::final_name()
 {
-	auto full_name = this->full_name();
-	auto ret = full_name;
+	auto fullname = this->full_name();
+	auto ret = fullname;
 
 	if(this->decompress) {
-		auto position = full_name.rfind(".");
+		auto position = fullname.rfind(".");
 		if(position != std::string::npos) {
-			ret = full_name.substr(0, position);
+			ret = fullname.substr(0, position);
 		}
 	}
 
@@ -123,8 +123,8 @@ bool DownloadFetch::fetch(BuildDir *d)
 		if(decompress) {
 			// We want to run a command on this file
 			std::string cmd = std::string();
-			char *filename = strdup(fname.c_str());
-			char *ext = strrchr(filename, '.');
+			char *_filename = strdup(fname.c_str());
+			char *ext = strrchr(_filename, '.');
 			if(ext == NULL) {
 				log(P->getName().c_str(),
 				    "Could not guess decompression based on extension: %s\n",
@@ -137,7 +137,7 @@ bool DownloadFetch::fetch(BuildDir *d)
 				cmd = string_format("gunzip -d dl/%s", fullname.c_str());
 			}
 			std::system(cmd.c_str());
-			free(filename);
+			free(_filename);
 		}
 	}
 
@@ -146,11 +146,11 @@ bool DownloadFetch::fetch(BuildDir *d)
 	if(this->hash.length() != 0) {
 		std::string fpath = string_format(
 		    "%s/dl/%s", d->getWorld()->getWorkingDir().c_str(), this->final_name().c_str());
-		std::string hash = hash_file(fpath);
+		std::string _hash = hash_file(fpath);
 
-		if(strcmp(this->hash.c_str(), hash.c_str()) != 0) {
+		if(strcmp(this->hash.c_str(), _hash.c_str()) != 0) {
 			log(this->P, "Hash mismatched for %s\n(committed to %s, providing %s)",
-			    this->final_name().c_str(), this->hash.c_str(), hash.c_str());
+			    this->final_name().c_str(), this->hash.c_str(), _hash.c_str());
 			ret = false;
 		}
 	}
@@ -161,14 +161,14 @@ bool DownloadFetch::fetch(BuildDir *d)
 std::string DownloadFetch::HASH()
 {
 	/* Check if the package contains pre-computed hashes */
-	std::string hash = P->getFileHash(this->final_name());
+	std::string _hash = P->getFileHash(this->final_name());
 	/* Otherwise fetch and calculate the hash */
-	if(hash.empty()) {
+	if(_hash.empty()) {
 		log(P, "No hash for %s in package/%s/Digest", this->final_name().c_str(),
 		    P->getName().c_str());
 		throw CustomException("Missing hash " + P->getName());
 	}
-	this->hash = hash;
+	this->hash = _hash;
 	return this->hash;
 }
 

include/buildsys.h
@@ -392,7 +392,8 @@ namespace buildsys
 		 *  \param path The path to run this command in
 		 *  \param app The program to invoke
 		 */
-		PackageCmd(const std::string &path, const std::string &app) : path(path), app(app)
+		PackageCmd(const std::string &_path, const std::string &_app)
+		    : path(_path), app(_app)
 		{
 			this->addArg(app);
 		};
@@ -439,7 +440,7 @@ namespace buildsys
 		mutable std::mutex lock;
 
 	public:
-		explicit DLObject(std::string filename) : filename(filename)
+		explicit DLObject(std::string _filename) : filename(_filename)
 		{
 		}
 		std::string fileName()
@@ -450,9 +451,9 @@ namespace buildsys
 		{
 			return this->hash;
 		}
-		void setHASH(std::string hash)
+		void setHASH(std::string _hash)
 		{
-			this->hash = hash;
+			this->hash = _hash;
 		}
 		std::mutex &getLock()
 		{
@@ -483,7 +484,7 @@ namespace buildsys
 		bool fetched;
 
 	public:
-		FetchUnit(std::string uri, Package *P) : fetch_uri(uri), P(P)
+		FetchUnit(std::string uri, Package *_P) : fetch_uri(uri), P(_P)
 		{
 		}
 		FetchUnit()
@@ -521,8 +522,9 @@ namespace buildsys
 		std::string final_name();
 
 	public:
-		DownloadFetch(std::string uri, bool decompress, std::string filename, Package *P)
-		    : FetchUnit(uri, P), decompress(decompress), filename(filename)
+		DownloadFetch(std::string _uri, bool _decompress, std::string _filename,
+		              Package *_P)
+		    : FetchUnit(_uri, _P), decompress(_decompress), filename(_filename)
 		{
 		}
 		virtual bool fetch(BuildDir *d);
@@ -538,7 +540,7 @@ namespace buildsys
 	class LinkFetch : public FetchUnit
 	{
 	public:
-		LinkFetch(std::string uri, Package *P) : FetchUnit(uri, P)
+		LinkFetch(std::string uri, Package *_P) : FetchUnit(uri, _P)
 		{
 		}
 		virtual bool fetch(BuildDir *d);
@@ -555,7 +557,7 @@ namespace buildsys
 	class CopyFetch : public FetchUnit
 	{
 	public:
-		CopyFetch(std::string uri, Package *P) : FetchUnit(uri, P)
+		CopyFetch(std::string uri, Package *_P) : FetchUnit(uri, _P)
 		{
 		}
 		virtual bool fetch(BuildDir *d);
@@ -676,7 +678,8 @@ namespace buildsys
 
 	public:
 		PatchExtractionUnit(int level, const std::string &patch_path,
-		                    const std::string &patch_fname, const std::string &fname_short);
+		                    const std::string &patch_fname,
+		                    const std::string &_fname_short);
 		virtual ~PatchExtractionUnit()
 		{
 		}
@@ -700,7 +703,7 @@ namespace buildsys
 		std::string fname_short;
 
 	public:
-		FileCopyExtractionUnit(const std::string &fname, const std::string &fname_short);
+		FileCopyExtractionUnit(const std::string &fname, const std::string &_fname_short);
 		virtual bool print(std::ostream &out)
 		{
 			out << this->type() << " " << this->fname_short << " " << this->HASH()
@@ -722,7 +725,7 @@ namespace buildsys
 		FetchUnit *fetched;
 
 	public:
-		FetchedFileCopyExtractionUnit(FetchUnit *fetch, const std::string &fname_short);
+		FetchedFileCopyExtractionUnit(FetchUnit *fetch, const std::string &_fname_short);
 		virtual bool print(std::ostream &out)
 		{
 			out << this->type() << " " << this->fname_short << " " << this->HASH()
@@ -834,8 +837,9 @@ namespace buildsys
 		World *WORLD;
 
 	public:
-		FeatureValueUnit(World *WORLD, const std::string &feature, const std::string &value)
-		    : feature(feature), value(value), WORLD(WORLD)
+		FeatureValueUnit(World *_WORLD, const std::string &_feature,
+		                 const std::string &_value)
+		    : feature(_feature), value(_value), WORLD(_WORLD)
 		{
 		}
 		virtual bool print(std::ostream &out);
@@ -852,7 +856,7 @@ namespace buildsys
 		std::string feature;
 
 	public:
-		explicit FeatureNilUnit(const std::string &feature) : feature(feature)
+		explicit FeatureNilUnit(const std::string &_feature) : feature(_feature)
 		{
 		}
 		virtual bool print(std::ostream &out)
@@ -873,7 +877,7 @@ namespace buildsys
 		std::string uri;  //!< URI of this package file
 		std::string hash; //!< Hash of this package file
 	public:
-		PackageFileUnit(const std::string &fname, const std::string &fname_short);
+		PackageFileUnit(const std::string &fname, const std::string &_fname_short);
 		virtual bool print(std::ostream &out)
 		{
 			out << this->type() << " " << this->uri << " " << this->hash << std::endl;
@@ -892,7 +896,7 @@ namespace buildsys
 		std::string uri;  //!< URI of this package file
 		std::string hash; //!< Hash of this package file
 	public:
-		RequireFileUnit(const std::string &fname, const std::string &fname_short);
+		RequireFileUnit(const std::string &fname, const std::string &_fname_short);
 		virtual bool print(std::ostream &out)
 		{
 			out << this->type() << " " << this->uri << " " << this->hash << std::endl;
@@ -1061,7 +1065,7 @@ namespace buildsys
 		World *WORLD;
 
 	public:
-		NameSpace(World *W, std::string name) : name(name), WORLD(W)
+		NameSpace(World *_W, std::string _name) : name(_name), WORLD(_W)
 		{
 		}
 		~NameSpace();
@@ -1090,7 +1094,7 @@ namespace buildsys
 
 	public:
 		//! Create a package dependency
-		PackageDepend(Package *p, bool locally) : p(p), locally(locally)
+		PackageDepend(Package *_p, bool _locally) : p(_p), locally(_locally)
 		{
 		}
 		~PackageDepend()
@@ -1186,10 +1190,10 @@ namespace buildsys
 		 *  \param name The name of this package
 		 *  \param file The lua file describing this package
 		 */
-		Package(NameSpace *ns, std::string name, std::string file_short, std::string file,
-		        std::string overlay)
-		    : name(name), file(file), file_short(file_short), overlay(overlay),
-		      buildinfo_hash(""), ns(ns), bd(new BuildDir(this)), f(new Fetch()),
+		Package(NameSpace *_ns, std::string _name, std::string _file_short,
+		        std::string _file, std::string _overlay)
+		    : name(_name), file(_file), file_short(_file_short), overlay(_overlay),
+		      buildinfo_hash(""), ns(_ns), bd(new BuildDir(this)), f(new Fetch()),
 		      Extract(new Extraction()), build_description(new BuildDescription()),
 		      lua(new Lua()), intercept(false), depsExtraction(""),
 		      depsExtractionDirectOnly(false), visiting(false), processing_queued(false),
@@ -1221,11 +1225,11 @@ namespace buildsys
 			}
 		};
 		//! Set the namespace this package is in
-		void setNS(NameSpace *ns)
+		void setNS(NameSpace *_ns)
 		{
 			if(this->ns)
 				this->ns->removePackage(this);
-			this->ns = ns;
+			this->ns = _ns;
 			if(this->ns)
 				this->ns->addPackage(this);
 			this->resetBD();
@@ -1534,16 +1538,16 @@ namespace buildsys
 		DLObject *_findDLObject(std::string);
 
 	public:
-		explicit World(char *bsapp)
-		    : bsapp(std::string(bsapp)), features(new key_value()),
+		explicit World(char *_bsapp)
+		    : bsapp(std::string(_bsapp)), features(new key_value()),
 		      forcedDeps(new string_list()), namespaces(new std::list<NameSpace *>()),
 		      dlobjects(new std::list<DLObject *>()), overlays(new string_list()),
 		      ignoredFeatures(new string_list())
 		{
 			overlays->push_back(std::string("."));
-			char *pwd = getcwd(NULL, 0);
-			this->pwd = std::string(pwd);
-			free(pwd);
+			char *_pwd = getcwd(NULL, 0);
+			this->pwd = std::string(_pwd);
+			free(_pwd);
 		};
 
 		~World();

namespace.cpp
@@ -58,7 +58,7 @@ std::string NameSpace::getInstallDir()
 	return res.str();
 }
 
-Package *NameSpace::findPackage(std::string name)
+Package *NameSpace::findPackage(std::string _name)
 {
 	std::string file;
 	std::string file_short;
@@ -69,7 +69,7 @@ Package *NameSpace::findPackage(std::string name)
 	std::list<Package *>::iterator iter = this->packagesStart();
 	std::list<Package *>::iterator iterEnd = this->packagesEnd();
 	for(; iter != iterEnd; iter++) {
-		if((*iter)->getName().compare(name) == 0) {
+		if((*iter)->getName().compare(_name) == 0) {
 			return (*iter);
 		}
 	}
@@ -78,7 +78,7 @@ Package *NameSpace::findPackage(std::string name)
 	{
 		// check that the dependency exists
 		std::string lua_file("");
-		char *dependPath = strdup(name.c_str());
+		char *dependPath = strdup(_name.c_str());
 		char *lastPart = strrchr(dependPath, '/');
 		bool found = false;
 
@@ -90,14 +90,14 @@ Package *NameSpace::findPackage(std::string name)
 
 		std::string relative_fname =
 		    string_format("package/%s/%s.lua", dependPath, lastPart);
-		string_list::iterator iter = this->WORLD->overlaysStart();
-		string_list::iterator iterEnd = this->WORLD->overlaysEnd();
-		for(; iter != iterEnd; iter++) {
-			lua_file = string_format("%s/%s", iter->c_str(), relative_fname.c_str());
+		string_list::iterator _iter = this->WORLD->overlaysStart();
+		string_list::iterator _iterEnd = this->WORLD->overlaysEnd();
+		for(; _iter != _iterEnd; _iter++) {
+			lua_file = string_format("%s/%s", _iter->c_str(), relative_fname.c_str());
 			FILE *f = fopen(lua_file.c_str(), "r");
 			if(f != NULL) {
 				found = true;
-				overlay = *iter;
+				overlay = *_iter;
 				fclose(f);
 			}
 			if(found) {
@@ -106,7 +106,7 @@ Package *NameSpace::findPackage(std::string name)
 		}
 
 		if(!found) {
-			throw CustomException("Package not found: " + name);
+			throw CustomException("Package not found: " + _name);
 		}
 
 		free(dependPath);
@@ -116,7 +116,7 @@ Package *NameSpace::findPackage(std::string name)
 		file_short = relative_fname;
 	}
 
-	Package *p = new Package(this, name, file_short, file, overlay);
+	Package *p = new Package(this, _name, file_short, file, overlay);
 	this->_addPackage(p);
 
 	return p;

package.cpp
@@ -393,9 +393,9 @@ void Package::prepareBuildInfo()
 
 	// Create the new build info file
 	std::string buildInfoFname = this->bd->getPath() + "/.build.info.new";
-	std::ofstream buildInfo(buildInfoFname.c_str());
-	this->build_description->print(buildInfo);
-	buildInfo.close();
+	std::ofstream _buildInfo(buildInfoFname.c_str());
+	this->build_description->print(_buildInfo);
+	_buildInfo.close();
 	this->updateBuildInfoHash();
 	this->buildInfoPrepared = true;
 }
@@ -469,22 +469,22 @@ bool Package::shouldBuild(bool locally)
 	std::string fname = this->getWorld()->getWorkingDir() + "/output/" +
 	                    this->getNS()->getName() + "/install/" + this->name + ".cpio";
 
-	FILE *f = fopen(fname.c_str(), "r");
-	if(f == NULL) {
+	FILE *_f = fopen(fname.c_str(), "r");
+	if(_f == NULL) {
 		ret = true;
 	} else {
-		fclose(f);
+		fclose(_f);
 	}
 
 	// Now lets check that the staging file (still) exists
 	fname = this->getWorld()->getWorkingDir() + "/output/" + this->getNS()->getName() +
 	        "/staging/" + this->name + ".cpio";
 
-	f = fopen(fname.c_str(), "r");
-	if(f == NULL) {
+	_f = fopen(fname.c_str(), "r");
+	if(_f == NULL) {
 		ret = true;
 	} else {
-		fclose(f);
+		fclose(_f);
 	}
 
 	std::string cmd =

commit c4b7505ab946a412c87a0105d61d12d69e45e588

git: Fix NULL pointer dereference
We are currently using the uninitialised pointer to the package on
the extraction unit. We should be using the pointer passed into the
constructor.

fixes: commit 3a4313d ("Enable -Wshadow compiler flag")

Change-Id: I4454010eeb6ff614cd4a0902dad922964d63aa51
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

extraction/git.cpp
@@ -131,7 +131,7 @@ GitExtractionUnit::GitExtractionUnit(const std::string &remote, const std::strin
                                      std::string _refspec, Package *_P)
 {
 	this->uri = remote;
-	this->local = P->getWorld()->getWorkingDir() + "/source/" + _local;
+	this->local = _P->getWorld()->getWorkingDir() + "/source/" + _local;
 	this->refspec = std::move(_refspec);
 	this->P = _P;
 	this->fetched = false;