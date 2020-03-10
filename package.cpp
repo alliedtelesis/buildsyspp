alliedtelesis/buildsyspp

commit a91cb4e529839cab68d13c2aad139db782f721a8

Remove iterator usage
Use for-range loops instead.

Change-Id: I276b6a07d908e9bf16fa6923c229f33eea0aa5d9
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

package.cpp
@@ -26,16 +26,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "include/buildsys.h"
 #include "interface/luainterface.h"
 
-std::list<PackageDepend>::iterator Package::dependsStart()
-{
-	return this->depends.begin();
-}
-
-std::list<PackageDepend>::iterator Package::dependsEnd()
-{
-	return this->depends.end();
-}
-
 BuildDir *Package::builddir()
 {
 	return &this->bd;
@@ -72,27 +62,25 @@ std::string Package::relative_fetch_path(const std::string &location, bool also_
 	if(location.at(0) == '/' || location.find("dl/") == 0) {
 		src_path = location;
 	} else {
-		auto iter = this->getWorld()->overlaysStart();
-		auto end = this->getWorld()->overlaysEnd();
 		bool exists(false);
 
 		if(location.at(0) == '.') {
-			for(; iter != end; iter++) {
-				src_path = *iter + "/" + location;
+			for(const auto &ov : this->getWorld()->getOverlays()) {
+				src_path = ov + "/" + location;
 				if(filesystem::exists(src_path)) {
 					exists = true;
 					break;
 				}
 			}
 		} else {
-			for(; iter != end; iter++) {
-				src_path = *iter + "/package/" + this->getName() + "/" + location;
+			for(const auto &ov : this->getWorld()->getOverlays()) {
+				src_path = ov + "/package/" + this->getName() + "/" + location;
 				if(filesystem::exists(src_path)) {
 					exists = true;
 					break;
 				}
 				if(also_root) {
-					src_path = *iter + "/" + location;
+					src_path = ov + "/" + location;
 					if(filesystem::exists(src_path)) {
 						exists = true;
 						break;
@@ -172,11 +160,8 @@ bool Package::checkForDependencyLoops()
 
 	this->visiting = true;
 
-	auto iter = this->dependsStart();
-	auto end = this->dependsEnd();
-
-	for(; iter != end; iter++) {
-		if(!(*iter).getPackage()->checkForDependencyLoops()) {
+	for(auto &depend : this->depends) {
+		if(!depend.getPackage()->checkForDependencyLoops()) {
 			log(this, "Child failed dependency loop check");
 			return false;
 		}
@@ -200,11 +185,8 @@ bool Package::extract_staging(const std::string &dir, std::list<std::string> *do
 		}
 	}
 
-	auto dIt = this->dependsStart();
-	auto dEnds = this->dependsEnd();
-
-	for(; dIt != dEnds; dIt++) {
-		if(!(*dIt).getPackage()->extract_staging(dir, done)) {
+	for(auto &depend : this->depends) {
+		if(!depend.getPackage()->extract_staging(dir, done)) {
 			return false;
 		}
 	}
@@ -240,12 +222,9 @@ bool Package::extract_install(const std::string &dir, std::list<std::string> *do
 		}
 	}
 
-	auto dIt = this->dependsStart();
-	auto dEnds = this->dependsEnd();
-
 	if(includeChildren && !this->intercept) {
-		for(; dIt != dEnds; dIt++) {
-			if(!(*dIt).getPackage()->extract_install(dir, done, includeChildren)) {
+		for(auto &depend : this->depends) {
+			if(!depend.getPackage()->extract_install(dir, done, includeChildren)) {
 				return false;
 			}
 		}
@@ -290,16 +269,12 @@ bool Package::canBuild()
 		return true;
 	}
 
-	auto dIt = this->dependsStart();
-	auto dEnds = this->dependsEnd();
-
-	if(dIt != dEnds) {
-		for(; dIt != dEnds; dIt++) {
-			if(!(*dIt).getPackage()->isBuilt()) {
-				return false;
-			}
+	for(auto &depend : this->depends) {
+		if(!depend.getPackage()->isBuilt()) {
+			return false;
 		}
 	}
+
 	return true;
 }
 
@@ -361,11 +336,8 @@ void Package::prepareBuildInfo()
 	this->build_description.add(this->Extract.extractionInfo(&this->bd));
 
 	// Add each of our dependencies build info files
-	auto dIt = this->dependsStart();
-	auto dEnds = this->dependsEnd();
-
-	for(; dIt != dEnds; dIt++) {
-		std::unique_ptr<BuildUnit> bi = (*dIt).getPackage()->buildInfo();
+	for(auto &depend : this->depends) {
+		std::unique_ptr<BuildUnit> bi = depend.getPackage()->buildInfo();
 		if(bi == nullptr) {
 			log(this, "bi is nullptr :(");
 			exit(-1);
@@ -510,10 +482,8 @@ bool Package::prepareBuildDirs()
 	}
 
 	std::list<std::string> done;
-	auto dIt = this->dependsStart();
-	auto dEnds = this->dependsEnd();
-	for(; dIt != dEnds; dIt++) {
-		if(!(*dIt).getPackage()->extract_staging(staging_dir, &done)) {
+	for(auto &depend : this->depends) {
+		if(!depend.getPackage()->extract_staging(staging_dir, &done)) {
 			return false;
 		}
 	}
@@ -544,10 +514,8 @@ bool Package::extractInstallDepends()
 
 	log(this, "Extracting installed files from dependencies ...");
 	std::list<std::string> done;
-	auto dIt = this->dependsStart();
-	auto dEnds = this->dependsEnd();
-	for(; dIt != dEnds; dIt++) {
-		if(!(*dIt).getPackage()->extract_install(this->depsExtraction, &done,
+	for(auto &depend : this->depends) {
+		if(!depend.getPackage()->extract_install(this->depsExtraction, &done,
 		                                         !this->depsExtractionDirectOnly)) {
 			return false;
 		}
@@ -626,12 +594,9 @@ bool Package::build(bool locally)
 		return true;
 	}
 
-	auto dIt = this->dependsStart();
-	auto dEnds = this->dependsEnd();
-
 	/* Check our dependencies are already built, or build them */
-	for(; dIt != dEnds; dIt++) {
-		if(!(*dIt).getPackage()->build()) {
+	for(auto &depend : this->depends) {
+		if(!depend.getPackage()->build()) {
 			return false;
 		}
 	}
@@ -663,11 +628,10 @@ bool Package::build(bool locally)
 	}
 	// Need to check that any packages that need to have been built locally
 	// actually have been
-	dIt = this->dependsStart();
-	for(; dIt != dEnds; dIt++) {
-		if((*dIt).getLocally()) {
-			log((*dIt).getPackage(), "Build triggered by " + this->getName());
-			if(!(*dIt).getPackage()->build(true)) {
+	for(auto &depend : this->depends) {
+		if(depend.getLocally()) {
+			log(depend.getPackage(), "Build triggered by " + this->getName());
+			if(!depend.getPackage()->build(true)) {
 				return false;
 			}
 		}

Makefile
@@ -43,7 +43,8 @@ cpplint:
 
 tidy:
 	find \( -name "*.cpp" -o -name "*.h" \)  -not -path "*test/*" -exec clang-tidy \
-		-checks=*,-fuchsia-default-arguments,-cert-env33-c,-llvm-header-guard,-fuchsia-overloaded-operator,-google-build-using-namespace,-google-global-names-in-headers \
+		-checks=*,-fuchsia-default-arguments,-cert-env33-c,-llvm-header-guard,-fuchsia-overloaded-operator,\
+-google-build-using-namespace,-google-global-names-in-headers,-performance-inefficient-string-concatenation \
 		-quiet -extra-arg-before=-xc++ {} -- $(CPPFLAGS) $(shell pkg-config --cflags $(LUAVERSION)) \;
 
 .PHONY: test

graph.cpp
@@ -55,26 +55,26 @@ public:
 
 void Internal_Graph::fill(World *W)
 {
-	for(auto N = W->nameSpacesStart(); N != W->nameSpacesEnd(); N++) {
-		for(auto I = (*N).packagesStart(); I != (*N).packagesEnd(); I++) {
+	for(const auto &ns : W->getNameSpaces()) {
+		for(const auto package : ns.getPackages()) {
 			NodeVertexMap::iterator pos;
 			bool inserted;
-			boost::tie(pos, inserted) = Nodes.insert(std::make_pair(*I, Vertex()));
+			boost::tie(pos, inserted) = Nodes.insert(std::make_pair(package, Vertex()));
 			if(inserted) {
 				Vertex u = add_vertex(g);
 				pos->second = u;
-				NodeMap.insert(std::make_pair(u, *I));
+				NodeMap.insert(std::make_pair(u, package));
 			}
 		}
 	}
 
-	for(auto N = W->nameSpacesStart(); N != W->nameSpacesEnd(); N++) {
-		for(auto I = (*N).packagesStart(); I != (*N).packagesEnd(); I++) {
-			for(auto J = (*I)->dependsStart(); J != (*I)->dependsEnd(); J++) {
+	for(const auto &ns : W->getNameSpaces()) {
+		for(const auto &package : ns.getPackages()) {
+			for(auto &depend : package->getDepends()) {
 				Edge e;
 				bool inserted;
 				boost::tie(e, inserted) =
-				    add_edge(Nodes[(*I)], Nodes[(*J).getPackage()], g);
+				    add_edge(Nodes[(package)], Nodes[depend.getPackage()], g);
 			}
 		}
 	}
@@ -92,11 +92,6 @@ void Internal_Graph::topological()
 	this->c.clear();
 
 	topological_sort(this->g, std::back_inserter(c));
-
-	//      std::cout << "A topological ordering: ";
-	//      for ( container::iterator ii=c->begin(); ii!=c->end(); ++ii)
-	//              std::cout << ((*NodeMap)[*ii])->getName() << " ";
-	//      std::cout << std::endl;
 }
 
 void Internal_Graph::deleteNode(Package *p)
@@ -111,7 +106,6 @@ Package *Internal_Graph::topoNext()
 	for(auto &ii : c) {
 		Package *p = NodeMap[ii];
 		if(!(p->isBuilt()) && !(p->isBuilding()) && p->canBuild()) {
-			// fprintf(stderr, "(Possible) Next Pacakge: %s\n", p->getName().c_str());
 			n = p;
 		}
 	}

include/buildsys.h
@@ -1027,8 +1027,10 @@ namespace buildsys
 		{
 			return name;
 		};
-		std::list<Package *>::iterator packagesStart();
-		std::list<Package *>::iterator packagesEnd();
+		const std::list<Package *> &getPackages() const
+		{
+			return packages;
+		}
 		//! Find or create a package with the given name
 		Package *findPackage(const std::string &_name);
 		void addPackage(Package *p);
@@ -1328,10 +1330,10 @@ namespace buildsys
 			this->building = true;
 		}
 
-		//! Get the start iterator for the dependencies list
-		std::list<PackageDepend>::iterator dependsStart();
-		//! Get the end iterator for the dependencies list
-		std::list<PackageDepend>::iterator dependsEnd();
+		std::list<PackageDepend> &getDepends()
+		{
+			return this->depends;
+		}
 
 		/** Print the label for use on the graph
 		 * Prints the package name, number of commands to run, and time spent building
@@ -1503,7 +1505,7 @@ namespace buildsys
 		std::list<NameSpace> namespaces;
 		std::list<DLObject> dlobjects;
 		Package *p{nullptr};
-		string_list overlays;
+		std::list<std::string> overlays;
 		Internal_Graph graph;
 		Internal_Graph topo_graph;
 		std::string fetch_from;
@@ -1527,7 +1529,7 @@ namespace buildsys
 	public:
 		explicit World(std::string _bsapp) : bsapp(std::move(_bsapp))
 		{
-			overlays.push_back(std::string("."));
+			overlays.emplace_back(std::string("."));
 			char *_pwd = getcwd(nullptr, 0);
 			this->pwd = std::string(_pwd);
 			free(_pwd); // NOLINT
@@ -1668,16 +1670,12 @@ namespace buildsys
 		//! Start the processing and building steps with the given meta package
 		bool basePackage(const std::string &filename);
 
-		//! Get the start iterator for the namespace list
-		std::list<NameSpace>::iterator nameSpacesStart()
+		//! Get the namespace list
+		const std::list<NameSpace> &getNameSpaces()
 		{
-			return this->namespaces.begin();
-		};
-		//! Get the end iterator for the namespace list
-		std::list<NameSpace>::iterator nameSpacesEnd()
-		{
-			return this->namespaces.end();
-		};
+			return this->namespaces;
+		}
+
 		//! Find (or create) a namespace
 		NameSpace *findNameSpace(const std::string &name);
 
@@ -1737,10 +1735,11 @@ namespace buildsys
 		{
 			this->overlays.push_back(path);
 		};
-		//! Get the start iterator for the overlay list
-		string_list::iterator overlaysStart();
-		//! Get the end iterator for the over list
-		string_list::iterator overlaysEnd();
+		//! Get the overlay list
+		const std::list<std::string> &getOverlays()
+		{
+			return this->overlays;
+		}
 		//! A thread has started
 		void threadStarted()
 		{

namespace.cpp
@@ -34,16 +34,6 @@ NameSpace::~NameSpace()
 	}
 }
 
-std::list<Package *>::iterator NameSpace::packagesStart()
-{
-	return this->packages.begin();
-}
-
-std::list<Package *>::iterator NameSpace::packagesEnd()
-{
-	return this->packages.end();
-}
-
 std::string NameSpace::getStagingDir()
 {
 	std::stringstream res;
@@ -66,11 +56,9 @@ Package *NameSpace::findPackage(const std::string &_name)
 
 	std::unique_lock<std::mutex> lk(this->lock);
 
-	auto iter = this->packagesStart();
-	auto iterEnd = this->packagesEnd();
-	for(; iter != iterEnd; iter++) {
-		if((*iter)->getName() == _name) {
-			return (*iter);
+	for(const auto package : this->getPackages()) {
+		if(package->getName() == _name) {
+			return package;
 		}
 	}
 
@@ -87,13 +75,11 @@ Package *NameSpace::findPackage(const std::string &_name)
 
 		bool found = false;
 		auto relative_fname = boost::format{"package/%1%/%2%.lua"} % _name % lastPart;
-		auto _iter = this->WORLD->overlaysStart();
-		auto _iterEnd = this->WORLD->overlaysEnd();
-		for(; _iter != _iterEnd; _iter++) {
-			lua_file = *_iter + "/" + relative_fname.str();
+		for(const auto &ov : this->WORLD->getOverlays()) {
+			lua_file = ov + "/" + relative_fname.str();
 			if(filesystem::exists(lua_file)) {
 				found = true;
-				overlay = *_iter;
+				overlay = ov;
 				break;
 			}
 		}

world.cpp
@@ -25,23 +25,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "include/buildsys.h"
 
-string_list::iterator World::overlaysStart()
-{
-	return this->overlays.begin();
-}
-
-string_list::iterator World::overlaysEnd()
-{
-	return this->overlays.end();
-}
-
 NameSpace *World::findNameSpace(const std::string &name)
 {
-	auto iter = this->nameSpacesStart();
-	auto iterEnd = this->nameSpacesEnd();
-	for(; iter != iterEnd; iter++) {
-		if((*iter).getName() == name) {
-			return &(*iter);
+	for(auto &ns : this->namespaces) {
+		if(ns.getName() == name) {
+			return &ns;
 		}
 	}
 
@@ -83,11 +71,8 @@ static void process_package(Package *p, PackageQueue *pq)
 		throw;
 	}
 
-	auto iter = p->dependsStart();
-	auto end = p->dependsEnd();
-
-	for(; iter != end; iter++) {
-		Package *dp = (*iter).getPackage();
+	for(auto &depend : p->getDepends()) {
+		Package *dp = depend.getPackage();
 		if(dp->setProcessingQueued()) {
 			pq->push(dp);
 		}

commit 104c1adf11f17c33cca7bcc19b56e10cb409270e

package: Remove shadowing
Older versions of GCC complain about this variable naming so fix it.

fixes: commit a91cb4e ("Remove iterator usage")

Change-Id: Iaffad9ae84880b420fd01da085769d28ce01c175
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

package.cpp
@@ -160,8 +160,8 @@ bool Package::checkForDependencyLoops()
 
 	this->visiting = true;
 
-	for(auto &depend : this->depends) {
-		if(!depend.getPackage()->checkForDependencyLoops()) {
+	for(auto &dp : this->depends) {
+		if(!dp.getPackage()->checkForDependencyLoops()) {
 			log(this, "Child failed dependency loop check");
 			return false;
 		}
@@ -185,8 +185,8 @@ bool Package::extract_staging(const std::string &dir, std::list<std::string> *do
 		}
 	}
 
-	for(auto &depend : this->depends) {
-		if(!depend.getPackage()->extract_staging(dir, done)) {
+	for(auto &dp : this->depends) {
+		if(!dp.getPackage()->extract_staging(dir, done)) {
 			return false;
 		}
 	}
@@ -223,8 +223,8 @@ bool Package::extract_install(const std::string &dir, std::list<std::string> *do
 	}
 
 	if(includeChildren && !this->intercept) {
-		for(auto &depend : this->depends) {
-			if(!depend.getPackage()->extract_install(dir, done, includeChildren)) {
+		for(auto &dp : this->depends) {
+			if(!dp.getPackage()->extract_install(dir, done, includeChildren)) {
 				return false;
 			}
 		}
@@ -269,8 +269,8 @@ bool Package::canBuild()
 		return true;
 	}
 
-	for(auto &depend : this->depends) {
-		if(!depend.getPackage()->isBuilt()) {
+	for(auto &dp : this->depends) {
+		if(!dp.getPackage()->isBuilt()) {
 			return false;
 		}
 	}
@@ -336,8 +336,8 @@ void Package::prepareBuildInfo()
 	this->build_description.add(this->Extract.extractionInfo(&this->bd));
 
 	// Add each of our dependencies build info files
-	for(auto &depend : this->depends) {
-		std::unique_ptr<BuildUnit> bi = depend.getPackage()->buildInfo();
+	for(auto &dp : this->depends) {
+		std::unique_ptr<BuildUnit> bi = dp.getPackage()->buildInfo();
 		if(bi == nullptr) {
 			log(this, "bi is nullptr :(");
 			exit(-1);
@@ -482,8 +482,8 @@ bool Package::prepareBuildDirs()
 	}
 
 	std::list<std::string> done;
-	for(auto &depend : this->depends) {
-		if(!depend.getPackage()->extract_staging(staging_dir, &done)) {
+	for(auto &dp : this->depends) {
+		if(!dp.getPackage()->extract_staging(staging_dir, &done)) {
 			return false;
 		}
 	}
@@ -514,9 +514,9 @@ bool Package::extractInstallDepends()
 
 	log(this, "Extracting installed files from dependencies ...");
 	std::list<std::string> done;
-	for(auto &depend : this->depends) {
-		if(!depend.getPackage()->extract_install(this->depsExtraction, &done,
-		                                         !this->depsExtractionDirectOnly)) {
+	for(auto &dp : this->depends) {
+		if(!dp.getPackage()->extract_install(this->depsExtraction, &done,
+		                                     !this->depsExtractionDirectOnly)) {
 			return false;
 		}
 	}
@@ -595,8 +595,8 @@ bool Package::build(bool locally)
 	}
 
 	/* Check our dependencies are already built, or build them */
-	for(auto &depend : this->depends) {
-		if(!depend.getPackage()->build()) {
+	for(auto &dp : this->depends) {
+		if(!dp.getPackage()->build()) {
 			return false;
 		}
 	}
@@ -628,10 +628,10 @@ bool Package::build(bool locally)
 	}
 	// Need to check that any packages that need to have been built locally
 	// actually have been
-	for(auto &depend : this->depends) {
-		if(depend.getLocally()) {
-			log(depend.getPackage(), "Build triggered by " + this->getName());
-			if(!depend.getPackage()->build(true)) {
+	for(auto &dp : this->depends) {
+		if(dp.getLocally()) {
+			log(dp.getPackage(), "Build triggered by " + this->getName());
+			if(!dp.getPackage()->build(true)) {
 				return false;
 			}
 		}