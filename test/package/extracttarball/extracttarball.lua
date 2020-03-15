a27858e8b6feb255d4bed80fddd00ea4a9adea79
Mark Wutzke <mwutzke@users.noreply.github.com>
2020-03-16 08:08:25 +1100

    MAINT: Allow tests to run for GitHub clones
    Change-Id: I3b21aecd557a63fc91cab58e98889888a24319ac
    Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
    Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

--- test/package/extracttarball/extracttarball.lua
+++ test/package/extracttarball/extracttarball.lua
@@ -1,4 +1,9 @@
 require('buildsys')
 
 bd = builddir()
-bd:extract(bd:fetch { method = 'dl', uri = 'http://awpbuild.atlnz.lc/release_tarballs/dbus-1.2.16.tar.gz' })
+bd:extract(
+    bd:fetch {
+        method = 'dl',
+        uri = 'https://dbus.freedesktop.org/releases/dbus/dbus-1.2.16.tar.gz',
+    }
+)
