alliedtelesis/buildsyspp

commit ec1fe90c0dcc0afc3005ff8ecea017a9db79cf7d

package: Extract packages in parallel
After commit 7453e80 ("package.cpp: abort extraction if there are existing files")
extracting in parallel should now work as expected. Therefore we add back
the ability to extract in parallel. Note that we now also respect the buildsys++
option used to limit thread usage.

Extraction in parallel now provides a noticeable speedup in build times.

Change-Id: I2f304e698c7283d32dbafc234d17b0f34eebe64d
Reviewed-by: Mark Wutzke <mark.wutzke@alliedtelesis.co.nz>
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

package.cpp
@@ -500,12 +500,25 @@ bool Package::prepareBuildDirs()
 
 	std::unordered_set<Package *> packages;
 	this->getAllDependedPackages(&packages);
-	bool result{true};
+
+	bool use_threads = this->getWorld()->getThreadsLimit() == 0;
+	std::list<std::thread> threads;
+	std::atomic<bool> result{true};
 	for(auto p : packages) {
-		result = p->extract_staging(this->bd.getStaging());
-		if(!result) {
-			break;
+		if(use_threads) {
+			std::thread th([p, this, &result] {
+				bool ret = p->extract_staging(this->bd.getStaging());
+				if(!ret) {
+					result = false;
+				}
+			});
+			threads.push_back(std::move(th));
+		} else {
+			result = p->extract_staging(this->bd.getStaging());
 		}
+	}
+	for(auto &t : threads) {
+		t.join();
 	}
 
 	if(result) {
@@ -539,12 +552,25 @@ bool Package::extractInstallDepends()
 
 	std::unordered_set<Package *> packages;
 	this->getDependedPackages(&packages, !this->depsExtractionDirectOnly, false);
-	bool result{true};
+
+	bool use_threads = this->getWorld()->getThreadsLimit() == 0;
+	std::list<std::thread> threads;
+	std::atomic<bool> result{true};
 	for(auto p : packages) {
-		result = p->extract_install(this->depsExtraction);
-		if(!result) {
-			break;
+		if(use_threads) {
+			std::thread th([p, this, &result] {
+				bool ret = p->extract_install(this->depsExtraction);
+				if(!ret) {
+					result = false;
+				}
+			});
+			threads.push_back(std::move(th));
+		} else {
+			result = p->extract_install(this->depsExtraction);
 		}
+	}
+	for(auto &t : threads) {
+		t.join();
 	}
 
 	if(result) {

include/buildsys.h
@@ -1750,6 +1750,11 @@ namespace buildsys
 		{
 			this->threads_limit = tl;
 		};
+		//! Get the thread limit value
+		int getThreadsLimit() const
+		{
+			return this->threads_limit;
+		}
 		//! output the dependency graph
 		bool output_graph()
 		{

commit 82b1ab33debd520ed9cbf4b3f966558621db79b1

package: Stop extracting when extraction fails
In the case we are extracting serially we should stop extraction
if any of the extractions fail.

fixes: commit ec1fe90 ("package: Extract packages in parallel")

Change-Id: Ic81c7c5e5a5cc2288ca2c56e07a3baf0c8bdfce5
Reviewed-by: Mark Wutzke <mark.wutzke@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

package.cpp
@@ -515,6 +515,9 @@ bool Package::prepareBuildDirs()
 			threads.push_back(std::move(th));
 		} else {
 			result = p->extract_staging(this->bd.getStaging());
+			if (!result) {
+				break;
+			}
 		}
 	}
 	for(auto &t : threads) {
@@ -567,6 +570,9 @@ bool Package::extractInstallDepends()
 			threads.push_back(std::move(th));
 		} else {
 			result = p->extract_install(this->depsExtraction);
+			if (!result) {
+				break;
+			}
 		}
 	}
 	for(auto &t : threads) {