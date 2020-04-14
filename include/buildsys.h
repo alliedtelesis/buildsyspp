alliedtelesis/buildsyspp

commit b34bcba4eb882352d7e6250c8dc4599a7cbdb96e

BuildDir: Simplify construction
Take the required strings, instead of the Package class. This will
make unit testing this class easier.

Change-Id: I0ab4441082d3066a54aff53276fa31c1f932dfa8
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Reviewed-by: Mark Wutzke <mark.wutzke@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

include/buildsys.h
@@ -300,9 +300,12 @@ namespace buildsys
 
 	public:
 		/** Create a build directory
-		 *  \param P The package this directory is for
+		 *  @param pwd - The current working directory
+		 *  @param gname - The namespace name of the package
+		 *  @param pname - The package name
 		 */
-		explicit BuildDir(Package *P);
+		BuildDir(const std::string &pwd, const std::string &gname,
+		         const std::string &pname);
 		//! Return the full path to this directory
 		const std::string &getPath() const
 		{
@@ -1123,7 +1126,7 @@ namespace buildsys
 		        std::string _file, std::string _overlay, std::string _pwd)
 		    : name(std::move(_name)), file(std::move(_file)),
 		      file_short(std::move(_file_short)), overlay(std::move(_overlay)),
-		      pwd(std::move(_pwd)), ns(_ns), bd(BuildDir(this))
+		      pwd(std::move(_pwd)), ns(_ns), bd(BuildDir(_pwd, _ns->getName(), _name))
 		{
 		}
 		//! Set the namespace this package is in
@@ -1136,7 +1139,7 @@ namespace buildsys
 			if(this->ns != nullptr) {
 				this->ns->addPackage(this);
 			}
-			this->bd = BuildDir(this);
+			this->bd = BuildDir(this->getPwd(), _ns->getName(), this->name);
 		};
 		//! Returns the namespace this package is in
 		NameSpace *getNS()

dir/builddir.cpp
@@ -29,12 +29,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sys/stat.h>
 #include <sys/types.h>
 
-BuildDir::BuildDir(Package *P)
+BuildDir::BuildDir(const std::string &pwd, const std::string &gname,
+                   const std::string &pname)
 {
-	std::string gname = P->getNS()->getName();
-	std::string pname = P->getName();
-	std::string pwd = P->getPwd();
-
 	filesystem::create_directories("output");
 	filesystem::create_directories("output/" + gname);
 	filesystem::create_directories("output/" + gname + "/staging");

commit 002a37993dcb5af9ecc1d69da2aaeac5e672865b

BuildDir: Don't use moved strings
These strings have previously been moved into other values hence
we should not be using them.

fixes: commit b34bcba ("BuildDir: Simplify construction")

Change-Id: I11f44950edeecd3210664c2a11c583b80e30af37
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

include/buildsys.h
@@ -1126,7 +1126,8 @@ namespace buildsys
 		        std::string _file, std::string _overlay, std::string _pwd)
 		    : name(std::move(_name)), file(std::move(_file)),
 		      file_short(std::move(_file_short)), overlay(std::move(_overlay)),
-		      pwd(std::move(_pwd)), ns(_ns), bd(BuildDir(_pwd, _ns->getName(), _name))
+		      pwd(std::move(_pwd)), ns(_ns),
+		      bd(BuildDir(this->pwd, _ns->getName(), this->name))
 		{
 		}
 		//! Set the namespace this package is in