alliedtelesis/buildsyspp

commit 74c1d1369cd2075bc6cf9f7663b049a1c868f309

packagecmd: Take logger instead of package
Change-Id: Iba3779bb5b34273e1f6e431c12d7575c7e0b6e71
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

packagecmd.cpp
@@ -43,12 +43,8 @@ PackageCmd::PackageCmd(std::string _path, std::string _app)
 	}
 }
 
-static void pipe_data(int fd, Package *P)
+static void pipe_data_thread(Logger *logger, int fd)
 {
-	if(P == nullptr) {
-		return;
-	}
-
 	// get the data until there is a breakchar ...
 	std::string recv_buf;
 	char recv_byte;
@@ -57,111 +53,103 @@ static void pipe_data(int fd, Package *P)
 		ssize_t res = read(fd, reinterpret_cast<void *>(&recv_byte), 1); // NOLINT
 		if(res <= 0) {
 			if(!recv_buf.empty()) {
-				P->program_output(recv_buf);
+				logger->program_output(recv_buf);
 			}
 			break;
 		}
 		if(recv_byte == '\n') {
 			recv_buf.push_back('\0');
 			// Print the line, clear the string and start again
-			P->program_output(recv_buf);
+			logger->program_output(recv_buf);
 			recv_buf.clear();
 		} else {
 			recv_buf.push_back(recv_byte);
 		}
 	}
-}
-
-static void pipe_data_thread(Package *P, int fd)
-{
-	pipe_data(fd, P);
 	close(fd);
 }
 
-static void exec_process(const std::string &program,
-                         const std::vector<std::string> &args = {},
-                         const std::vector<std::string> &env = {})
+int PackageCmd::exec_process(Logger *logger, const std::vector<int> *pipe_fds)
 {
-	std::vector<const char *> pargs(args.size());
-	std::vector<const char *> penv(env.size());
-
-	std::transform(args.begin(), args.end(), pargs.begin(),
-	               [](const std::string &s) { return s.c_str(); });
-	pargs.push_back(nullptr);
-
-	std::transform(env.begin(), env.end(), penv.begin(),
-	               [](const std::string &s) { return s.c_str(); });
-	penv.push_back(nullptr);
-
-	execvpe(program.c_str(), const_cast<char *const *>(pargs.data()), // NOLINT
-	        const_cast<char *const *>(penv.data()));                  // NOLINT
-}
-
-static int run(Package *P, const std::string &program, const std::vector<std::string> &argv,
-               const std::string &path, const std::vector<std::string> &newenvp,
-               bool log_output)
-{
-	std::vector<int> fds(2);
-	if(log_output) {
-		int res = pipe(&fds[0]);
-
-		if(res != 0) {
-			P->log("pipe() failed: " + std::string(strerror(errno)));
-		}
-
-		std::thread thr(pipe_data_thread, P, fds[0]);
-		thr.detach();
-	}
-	// call the program ...
 	int pid = fork();
 	if(pid < 0) { // something bad happened ...
-		P->log("fork() failed: " + std::string(strerror(errno)));
+		logger->log("fork() failed: " + std::string(strerror(errno)));
 		exit(-1);
-	} else if(pid == 0) { // child process
-		if(log_output) {
-			close(fds[0]);
-			dup2(fds[1], STDOUT_FILENO);
-			dup2(fds[1], STDERR_FILENO);
-			close(fds[1]);
+	} else if(pid == 0) { // Child process
+		if(pipe_fds != nullptr) {
+			close(pipe_fds->at(0));
+			dup2(pipe_fds->at(1), STDOUT_FILENO);
+			dup2(pipe_fds->at(1), STDERR_FILENO);
+			close(pipe_fds->at(1));
 		}
-		if(chdir(path.c_str()) != 0) {
-			P->log(boost::format{"chdir '%1%' failed"} % path);
+
+		if(chdir(this->path.c_str()) != 0) {
+			logger->log(boost::format{"chdir '%1%' failed"} % this->path);
 			exit(-1);
 		}
-		exec_process(program, argv, newenvp);
-		P->log("Failed Running " + program);
+
+		std::vector<const char *> pargs(this->args.size());
+		std::vector<const char *> penv(this->envp.size());
+
+		std::transform(this->args.begin(), this->args.end(), pargs.begin(),
+		               [](const std::string &s) { return s.c_str(); });
+		pargs.push_back(nullptr);
+
+		std::transform(this->envp.begin(), this->envp.end(), penv.begin(),
+		               [](const std::string &s) { return s.c_str(); });
+		penv.push_back(nullptr);
+
+		execvpe(this->app.c_str(), const_cast<char *const *>(pargs.data()), // NOLINT
+		        const_cast<char *const *>(penv.data()));                    // NOLINT
+
+		// Should never be reached
+		logger->log("Failed Running " + this->app);
 		exit(-1);
-	} else {
-		if(log_output) {
-			close(fds[1]);
+	}
+
+	// Parent process
+	return pid;
+}
+
+bool PackageCmd::Run(Logger *logger)
+{
+	int status = 0;
+
+	if(this->log_output) {
+		std::vector<int> pipe_fds(2);
+
+		int res = pipe(&pipe_fds[0]);
+
+		if(res != 0) {
+			logger->log("pipe() failed: " + std::string(strerror(errno)));
 		}
-		int status = 0;
+
+		int pid = this->exec_process(logger, &pipe_fds);
+
+		close(pipe_fds[1]);
+		std::thread thr(pipe_data_thread, logger, pipe_fds[0]);
+		thr.detach();
 		waitpid(pid, &status, 0);
-		// check return status ...
-		if(WEXITSTATUS(status) < 0) { // NOLINT
-			P->log(boost::format{"Error Running %1% (path = %2%, return code = %3%)"} %
-			       program % path % status);
-		}
-		return WEXITSTATUS(status); // NOLINT
-	}
-	return -1;
-}
-
-bool PackageCmd::Run(Package *P)
-{
-	bool res = true;
-	if(run(P, this->args[0], this->args, this->path, this->envp, this->log_output) != 0) {
-		this->printCmd();
-		res = false;
+	} else {
+		int pid = this->exec_process(logger, nullptr);
+		waitpid(pid, &status, 0);
 	}
 
-	return res;
+	// check return status ...
+	if(WEXITSTATUS(status) < 0) { // NOLINT
+		logger->log(boost::format{"Error Running %1% (path = %2%, return code = %3%)"} %
+		            this->app % this->path % status);
+		this->printCmd(logger);
+		return false;
+	}
+
+	return true;
 }
 
-void PackageCmd::printCmd() const
+void PackageCmd::printCmd(Logger *logger) const
 {
-	std::cout << "Path: " << this->path << std::endl;
+	logger->log("Path: " + this->path);
 	for(size_t i = 0; i < this->args.size(); i++) {
-		std::cout << boost::format{"Arg[%1%] = '%2%'"} % i % this->args[i] << std::endl;
+		logger->log(boost::format{"Arg[%1%] = '%2%'"} % i % this->args[i]);
 	}
 }

extraction.cpp
@@ -114,7 +114,7 @@ bool TarExtractionUnit::extract(Package *P)
 	pc.addArg("xf");
 	pc.addArg(P->getPwd() + "/" + this->uri);
 
-	if(!pc.Run(P)) {
+	if(!pc.Run(P->getLogger())) {
 		throw CustomException("Failed to extract file");
 	}
 
@@ -130,7 +130,7 @@ bool ZipExtractionUnit::extract(Package *P)
 	pc.addArg("-o");
 	pc.addArg(P->getPwd() + "/" + this->uri);
 
-	if(!pc.Run(P)) {
+	if(!pc.Run(P->getLogger())) {
 		throw CustomException("Failed to extract file");
 	}
 
@@ -168,12 +168,12 @@ bool PatchExtractionUnit::extract(Package *P)
 
 	pc_dry.addArg("--dry-run");
 
-	if(!pc_dry.Run(P)) {
+	if(!pc_dry.Run(P->getLogger())) {
 		P->log("Patch file: " + this->uri);
 		throw CustomException("Will fail to patch");
 	}
 
-	if(!pc.Run(P)) {
+	if(!pc.Run(P->getLogger())) {
 		throw CustomException("Truely failed to patch");
 	}
 
@@ -203,7 +203,7 @@ bool FileCopyExtractionUnit::extract(Package *P)
 
 	pc.addArg(".");
 
-	if(!pc.Run(P)) {
+	if(!pc.Run(P->getLogger())) {
 		throw CustomException("Failed to copy file");
 	}
 
@@ -240,7 +240,7 @@ bool FetchedFileCopyExtractionUnit::extract(Package *P)
 
 	pc.addArg(".");
 
-	if(!pc.Run(P)) {
+	if(!pc.Run(P->getLogger())) {
 		throw CustomException("Failed to copy file");
 	}
 

extraction/git.cpp
@@ -141,7 +141,7 @@ bool GitExtractionUnit::updateOrigin()
 		}
 		pc.addArg("origin");
 		pc.addArg(location);
-		if(!pc.Run(this->P)) {
+		if(!pc.Run(this->P->getLogger())) {
 			throw CustomException("Failed: git remote set-url origin");
 		}
 
@@ -149,7 +149,7 @@ bool GitExtractionUnit::updateOrigin()
 		pc.addArg("fetch");
 		pc.addArg("origin");
 		pc.addArg("--tags");
-		if(!pc.Run(this->P)) {
+		if(!pc.Run(this->P->getLogger())) {
 			throw CustomException("Failed: git fetch origin --tags");
 		}
 	}
@@ -178,7 +178,7 @@ bool GitExtractionUnit::fetch(BuildDir *) // NOLINT
 			pc.addArg("fetch");
 			pc.addArg("origin");
 			pc.addArg("--tags");
-			if(!pc.Run(this->P)) {
+			if(!pc.Run(this->P->getLogger())) {
 				throw CustomException("Failed: git fetch origin --tags");
 			}
 		}
@@ -187,7 +187,7 @@ bool GitExtractionUnit::fetch(BuildDir *) // NOLINT
 		pc.addArg("-n");
 		pc.addArg(location);
 		pc.addArg(source_dir);
-		if(!pc.Run(this->P)) {
+		if(!pc.Run(this->P->getLogger())) {
 			throw CustomException("Failed to git clone");
 		}
 	}
@@ -211,7 +211,7 @@ bool GitExtractionUnit::fetch(BuildDir *) // NOLINT
 			pc.addArg("-q");
 			pc.addArg("--detach");
 			pc.addArg(this->refspec);
-			if(!pc.Run(this->P)) {
+			if(!pc.Run(this->P->getLogger())) {
 				throw CustomException("Failed to checkout");
 			}
 		}
@@ -266,7 +266,7 @@ bool GitExtractionUnit::extract(Package *_P)
 	pc.addArg("-dpRuf");
 	pc.addArg(this->localPath());
 	pc.addArg(".");
-	if(!pc.Run(_P)) {
+	if(!pc.Run(_P->getLogger())) {
 		throw CustomException("Failed to checkout");
 	}
 
@@ -287,7 +287,7 @@ bool LinkGitDirExtractionUnit::extract(Package *P)
 	}
 	pc.addArg(this->toDir);
 
-	if(!pc.Run(P)) {
+	if(!pc.Run(P->getLogger())) {
 		throw CustomException("Operation failed");
 	}
 
@@ -307,7 +307,7 @@ bool CopyGitDirExtractionUnit::extract(Package *P)
 	}
 	pc.addArg(this->toDir);
 
-	if(!pc.Run(P)) {
+	if(!pc.Run(P->getLogger())) {
 		throw CustomException("Operation failed");
 	}
 

fetch.cpp
@@ -99,7 +99,7 @@ bool DownloadFetch::fetch(BuildDir *) // NOLINT
 			std::string url = this->P->getWorld()->tarballCache() + "/" + fname;
 			pc.addArg(url);
 			pc.addArg("-O" + fullname + ".tmp");
-			localCacheHit = pc.Run(this->P);
+			localCacheHit = pc.Run(this->P->getLogger());
 			if(localCacheHit) {
 				filesystem::rename("dl/" + fullname + ".tmp", "dl/" + fullname);
 			}
@@ -110,7 +110,7 @@ bool DownloadFetch::fetch(BuildDir *) // NOLINT
 			PackageCmd pc("dl", "wget");
 			pc.addArg(this->fetch_uri);
 			pc.addArg("-O" + fullname + ".tmp");
-			if(!pc.Run(this->P)) {
+			if(!pc.Run(this->P->getLogger())) {
 				throw CustomException("Failed to fetch file");
 			}
 			filesystem::rename("dl/" + fullname + ".tmp", "dl/" + fullname);
@@ -172,7 +172,7 @@ bool LinkFetch::fetch(BuildDir *d)
 	std::string l = P->relative_fetch_path(this->fetch_uri);
 	pc.addArg(l);
 	pc.addArg(".");
-	if(!pc.Run(this->P)) {
+	if(!pc.Run(this->P->getLogger())) {
 		// An error occured, try remove the file, then relink
 		PackageCmd rmpc(d->getPath(), "rm");
 		rmpc.addArg("-fr");
@@ -183,11 +183,11 @@ bool LinkFetch::fetch(BuildDir *d)
 		// Strip the directory from the file name
 		l2 = l2.substr(l2.rfind('/') + 1);
 		rmpc.addArg(l2);
-		if(!rmpc.Run(this->P)) {
+		if(!rmpc.Run(this->P->getLogger())) {
 			throw CustomException(
 			    "Failed to ln (symbolically), could not remove target first");
 		}
-		if(!pc.Run(this->P)) {
+		if(!pc.Run(this->P->getLogger())) {
 			throw CustomException(
 			    "Failed to ln (symbolically), even after removing target first");
 		}
@@ -221,7 +221,7 @@ bool CopyFetch::fetch(BuildDir *d)
 	std::string l = P->absolute_fetch_path(this->fetch_uri);
 	pc.addArg(l);
 	pc.addArg(".");
-	if(!pc.Run(this->P)) {
+	if(!pc.Run(this->P->getLogger())) {
 		throw CustomException("Failed to copy (recursively)");
 	}
 	P->log("Copied data in, considering code updated");

include/buildsys.h
@@ -358,6 +358,8 @@ namespace buildsys
 		std::vector<std::string> envp;
 		bool log_output{true};
 
+		int exec_process(Logger *logger, const std::vector<int> *pipe_fds);
+
 	public:
 		/** Create a Package Command
 		 *  \param path The path to run this command in
@@ -381,13 +383,15 @@ namespace buildsys
 			this->envp.push_back(env);
 		}
 
-		/** Run this command
-		 *  \param P The package to use in the command logging
+		/**
+		 * Run this command
+		 *
+		 * @param logger - The logger to log the command output with
 		 */
-		bool Run(Package *P);
+		bool Run(Logger *logger);
 
 		//! Print the command line
-		void printCmd() const;
+		void printCmd(Logger *logger) const;
 
 		void disableLogging()
 		{
@@ -1311,9 +1315,9 @@ namespace buildsys
 		{
 			this->logger.log(str);
 		}
-		void program_output(const std::string &mesg)
+		Logger *getLogger()
 		{
-			this->logger.program_output(mesg);
+			return &this->logger;
 		}
 	};
 

package.cpp
@@ -202,7 +202,7 @@ bool Package::extract_staging(const std::string &dir)
 	                  this->name + ".tar";
 	pc.addArg(arg);
 
-	if(!pc.Run(this)) {
+	if(!pc.Run(&this->logger)) {
 		this->log("Failed to extract staging_dir");
 		return false;
 	}
@@ -229,7 +229,7 @@ bool Package::extract_install(const std::string &dir)
 			pc.addArg(arg);
 			pc.addArg(*it);
 
-			if(!pc.Run(this)) {
+			if(!pc.Run(&this->logger)) {
 				this->log(boost::format{"Failed to copy %1% (for install)"} % *it);
 				return false;
 			}
@@ -243,7 +243,7 @@ bool Package::extract_install(const std::string &dir)
 		std::string arg = this->pwd + "/output/" + this->getNS()->getName() + "/install/" +
 		                  this->name + ".tar";
 		pc.addArg(arg);
-		if(!pc.Run(this)) {
+		if(!pc.Run(&this->logger)) {
 			this->log("Failed to extract install_dir");
 			return false;
 		}
@@ -549,7 +549,7 @@ bool Package::extractInstallDepends()
 	PackageCmd pc(this->pwd, "/bin/rm");
 	pc.addArg("-fr");
 	pc.addArg(this->depsExtraction);
-	if(!pc.Run(this)) {
+	if(!pc.Run(&this->logger)) {
 		this->log(boost::format{"Failed to remove %1% (pre-install)"} %
 		          this->depsExtraction);
 		return false;
@@ -604,7 +604,7 @@ bool Package::packageNewStaging()
 	pc.addArg(arg);
 	pc.addArg(".");
 
-	if(!pc.Run(this)) {
+	if(!pc.Run(&this->logger)) {
 		this->log("Failed to compress staging directory");
 		return false;
 	}
@@ -624,7 +624,7 @@ bool Package::packageNewInstall()
 			    this->pwd + "/output/" + this->getNS()->getName() + "/install/" + *it;
 			pc.addArg(arg);
 
-			if(!pc.Run(this)) {
+			if(!pc.Run(&this->logger)) {
 				this->log(boost::format{"Failed to copy install file (%1%)"} % *it);
 				return false;
 			}
@@ -640,7 +640,7 @@ bool Package::packageNewInstall()
 		pc.addArg(arg);
 		pc.addArg(".");
 
-		if(!pc.Run(this)) {
+		if(!pc.Run(&this->logger)) {
 			this->log("Failed to compress install directory");
 			return false;
 		}
@@ -740,7 +740,7 @@ bool Package::build(bool locally)
 
 	this->log("Running Commands");
 	for(; cIt != cEnd; cIt++) {
-		if(!(*cIt).Run(this)) {
+		if(!(*cIt).Run(&this->logger)) {
 			return false;
 		}
 	}

commit ab56af95949a844be05535b1a743039d7e20d0fa

PackageCmd: Ensure error is returned if underlying command fails
fixes: commit 74c1d13 ("packagecmd: Take logger instead of package")

Change-Id: Id2f06d89ad119b027691ed6509d8d824853e2a1e
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

packagecmd.cpp
@@ -156,7 +156,7 @@ bool PackageCmd::Run(Logger *logger)
 	}
 
 	// check return status ...
-	if(WEXITSTATUS(status) < 0) { // NOLINT
+	if(WEXITSTATUS(status) != 0) { // NOLINT
 		logger->log(boost::format{"Error Running %1% (path = %2%, return code = %3%)"} %
 		            this->app % this->path % status);
 		this->printCmd(logger);

unit-test/packagecmd_unittests.cpp
@@ -124,3 +124,18 @@ TEST_CASE_METHOD(PackageCmdTestsFixture,
 
 	filesystem::remove(file_path);
 }
+
+TEST_CASE_METHOD(PackageCmdTestsFixture,
+                 "Test Run() function returns false if underlying command failed", "")
+{
+	PackageCmd pc(".", "/bin/false");
+	pc.disableLogging();
+
+	Logger logger;
+
+	std::stringstream buffer;
+	std::cout.rdbuf(buffer.rdbuf());
+
+	bool ret = pc.Run(&logger);
+	REQUIRE(!ret);
+}