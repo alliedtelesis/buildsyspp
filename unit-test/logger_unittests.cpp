alliedtelesis/buildsyspp

commit 0f4ad29b5cf63ccaa1b66321ff4b286299cf05eb

Add unit tests for new Logger class
And subsequently refactor appropriately.

Change-Id: I73f07591eab86bfac8d67cdf8e444d037ef03d7e
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

logger.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- Copyright 2019 Allied Telesis Labs Ltd. All rights reserved.
+ Copyright 2020 Allied Telesis Labs Ltd. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -27,80 +27,124 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 using namespace buildsys;
 
-// VT100 terminal color escape sequences
-#define COLOR_NORMAL ""
-#define COLOR_RESET "\033[m"
-#define COLOR_BOLD "\033[1m"
-#define COLOR_RED "\033[31m"
-#define COLOR_GREEN "\033[32m"
-#define COLOR_YELLOW "\033[33m"
-#define COLOR_BLUE "\033[34m"
-#define COLOR_MAGENTA "\033[35m"
-#define COLOR_CYAN "\033[36m"
-#define COLOR_BOLD_RED "\033[1;31m"
-#define COLOR_BOLD_GREEN "\033[1;32m"
-#define COLOR_BOLD_YELLOW "\033[1;33m"
-#define COLOR_BOLD_BLUE "\033[1;34m"
-#define COLOR_BOLD_MAGENTA "\033[1;35m"
-#define COLOR_BOLD_CYAN "\033[1;36m"
-#define COLOR_BG_RED "\033[41m"
-#define COLOR_BG_GREEN "\033[42m"
-#define COLOR_BG_YELLOW "\033[43m"
-#define COLOR_BG_BLUE "\033[44m"
-#define COLOR_BG_MAGENTA "\033[45m"
-#define COLOR_BG_CYAN "\033[46m"
+constexpr const char *Logger::COLOUR_NORMAL;
+constexpr const char *Logger::COLOUR_RESET;
+constexpr const char *Logger::COLOUR_BOLD_RED;
+constexpr const char *Logger::COLOUR_BOLD_BLUE;
 
+/**
+ * Construct a Logger object. Log messages will be output to std::cout
+ * with no prefix.
+ */
 Logger::Logger()
 {
 	this->output = &std::cout;
+	this->output_supports_colour = (isatty(fileno(stdout)) == 1);
 }
 
-Logger::Logger(std::string _prefix) : prefix(std::move(_prefix))
+/**
+ * Construct a Logger object. Log messages will be output to std::cout
+ * with the given prefix.
+ *
+ * @param _prefix - The prefix to prepend to log messages.
+ */
+Logger::Logger(const std::string &_prefix)
 {
+	this->prefix = _prefix + ": ";
 	this->output = &std::cout;
+	this->output_supports_colour = (isatty(fileno(stdout)) == 1);
 }
 
-Logger::Logger(std::string _prefix, const std::string &file_path)
-    : prefix(std::move(_prefix))
+/**
+ * Construct a Logger object. Log messages will be output to the given
+ * file path with the given prefix.
+ *
+ * @param _prefix - The prefix to prepend to log messages.
+ * @param file_path - The file to output the log messages to.
+ */
+Logger::Logger(const std::string &_prefix, const std::string &file_path)
 {
+	this->prefix = _prefix + ": ";
 	this->file_output = std::make_unique<std::ofstream>(file_path);
 	this->output = this->file_output.get();
 }
 
+/**
+ * Output a log message.
+ *
+ * @param str - The string to output.
+ */
 void Logger::log(const std::string &str)
 {
-	auto msg = boost::format{"%1%: %2%"} % this->prefix % str;
+	auto msg = boost::format{"%1%%2%"} % this->prefix % str;
 	*this->output << msg << std::endl;
 }
 
+/**
+ * Output a log message.
+ *
+ * @param str - The boost::format object containing the string to output.
+ */
 void Logger::log(const boost::format &str)
 {
 	this->log(str.str());
 }
 
-static inline const char *get_color(const std::string &mesg)
+/**
+ * Get the colour code required for the given log message. Messages that
+ * start with "error:" should print in red, while messages that begin
+ * with "warning:" should output in blue. All other messages should not
+ * have a colour.
+ *
+ * @param str - The log message to get the colour for.
+ *
+ * @returns The colour to print the message in.
+ */
+const char *Logger::get_colour(const std::string &str)
 {
-	if(mesg.find("error:") != std::string::npos) {
-		return COLOR_BOLD_RED;
-	}
-	if(mesg.find("warning:") != std::string::npos) {
-		return COLOR_BOLD_BLUE;
+	const std::string error_str("error:");
+	const std::string warning_str("warning:");
+
+	if(this->output_supports_colour) {
+		if(str.compare(0, error_str.size(), error_str) == 0) {
+			return Logger::COLOUR_BOLD_RED;
+		}
+		if(str.compare(0, warning_str.size(), warning_str) == 0) {
+			return Logger::COLOUR_BOLD_BLUE;
+		}
 	}
 
-	return nullptr;
+	return Logger::COLOUR_NORMAL;
 }
 
-void Logger::program_output(const std::string &mesg)
+/**
+ * Output the given message (program output). If the output supports colour
+ * (i.e. a terminal) then the message will be coloured if it is an error or
+ * warning message.
+ *
+ * @param str - The log message to print.
+ */
+void Logger::program_output(const std::string &str)
 {
-	static int isATTY = isatty(fileno(stdout));
-	const char *color;
-	boost::format msg;
-	bool output_is_cout = (this->output == &std::cout);
+	const char *colour = this->get_colour(str);
 
-	if(output_is_cout && (isATTY != 0) && ((color = get_color(mesg)) != nullptr)) {
-		msg = boost::format{"%1%: %2%%3%%4%"} % this->prefix % color % mesg % COLOR_RESET;
+	if(colour != Logger::COLOUR_NORMAL) {
+		auto coloured_str =
+		    boost::format{"%1%%2%%3%"} % colour % str % Logger::COLOUR_RESET;
+		this->log(coloured_str);
 	} else {
-		msg = boost::format{"%1%: %2%"} % this->prefix % mesg;
+		this->log(str);
 	}
-	*this->output << msg << std::endl;
+}
+
+/**
+ * Force the Logger object to output in colour.
+ *
+ * NOTE: This should only be used by the unit-tests.
+ *
+ * @param set - Whether to enable/disable colour output.
+ */
+void Logger::force_colour_output(bool set)
+{
+	this->output_supports_colour = set;
 }

unit-test/logger_unittests.cpp
@@ -1,0 +1,128 @@
+#define CATCH_CONFIG_MAIN
+
+#include "include/filesystem.h"
+#include "logger.hpp"
+#include <catch2/catch.hpp>
+
+using namespace buildsys;
+
+class LoggerTestsFixture
+{
+protected:
+	std::streambuf *coutbuf{nullptr};
+
+public:
+	LoggerTestsFixture()
+	{
+		// Some of the tests may redirect std::cout. Ensure that we restore it
+		// at the end of each test.
+		this->coutbuf = std::cout.rdbuf();
+	}
+	~LoggerTestsFixture()
+	{
+		std::cout.rdbuf(this->coutbuf);
+	}
+};
+
+TEST_CASE_METHOD(LoggerTestsFixture, "Test Logger default constructor", "")
+{
+	Logger logger;
+
+	std::stringstream buffer;
+	// Redirect std::cout so we can verify what is printed.
+	std::cout.rdbuf(buffer.rdbuf());
+
+	std::string test_string("This is a test log message");
+	logger.log(test_string);
+	REQUIRE(buffer.str() == test_string + "\n");
+
+	buffer.str("");
+
+	auto test_format = boost::format{"Check %1%"} % test_string;
+	logger.log(test_format);
+	REQUIRE(buffer.str() == "Check " + test_string + "\n");
+}
+
+TEST_CASE_METHOD(LoggerTestsFixture, "Test Logger prefix only constructor", "")
+{
+	std::string test_prefix("test_prefix");
+	Logger logger(test_prefix);
+
+	std::stringstream buffer;
+	// Redirect std::cout so we can verify what is printed.
+	std::cout.rdbuf(buffer.rdbuf());
+
+	std::string test_string("This is a test log message");
+	logger.log(test_string);
+	REQUIRE(buffer.str() == test_prefix + ": " + test_string + "\n");
+}
+
+TEST_CASE_METHOD(LoggerTestsFixture, "Test Logger prefix and file path constructor", "")
+{
+	std::string file_path = "test_output_file.txt";
+	std::string test_prefix("test_prefix");
+	Logger logger(test_prefix, file_path);
+
+	std::stringstream buffer;
+	// Redirect std::cout so we can verify what is printed.
+	std::cout.rdbuf(buffer.rdbuf());
+
+	std::string test_string("This is a test log message");
+	logger.log(test_string);
+
+	// Nothing should be printed to std::cout
+	REQUIRE(buffer.str() == "");
+
+	std::ifstream t(file_path);
+	std::stringstream file_contents;
+	file_contents << t.rdbuf();
+	REQUIRE(file_contents.str() == test_prefix + ": " + test_string + "\n");
+
+	filesystem::remove(file_path);
+}
+
+TEST_CASE_METHOD(LoggerTestsFixture, "Test Logger program_output() with coloured output",
+                 "")
+{
+	Logger logger;
+	logger.force_colour_output(true);
+
+	std::stringstream buffer;
+	std::cout.rdbuf(buffer.rdbuf());
+
+	std::string test_string("error: This is a test log message");
+	logger.program_output(test_string);
+
+	REQUIRE(buffer.str() == std::string(Logger::COLOUR_BOLD_RED) + test_string +
+	                            std::string(Logger::COLOUR_RESET) + "\n");
+
+	buffer.str("");
+
+	test_string = std::string("warning: This is a test log message");
+	logger.program_output(test_string);
+
+	REQUIRE(buffer.str() == std::string(Logger::COLOUR_BOLD_BLUE) + test_string +
+	                            std::string(Logger::COLOUR_RESET) + "\n");
+}
+
+TEST_CASE_METHOD(LoggerTestsFixture, "Test Logger program_output() without coloured output",
+                 "")
+{
+	Logger logger;
+	logger.force_colour_output(false);
+
+	std::stringstream buffer;
+	std::cout.rdbuf(buffer.rdbuf());
+
+	std::string test_string("error: This is a test log message");
+	logger.program_output(test_string);
+
+	REQUIRE(buffer.str() == test_string + "\n");
+
+	buffer.str("");
+
+	test_string = std::string("warning: This is a test log message");
+	logger.program_output(test_string);
+
+	REQUIRE(buffer.str() == test_string + "\n");
+}

logger.hpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- Copyright 2019 Allied Telesis Labs Ltd. All rights reserved.
+ Copyright 2020 Allied Telesis Labs Ltd. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -40,14 +40,24 @@ namespace buildsys
 		std::string prefix;
 		std::unique_ptr<std::ofstream> file_output;
 		std::ostream *output{nullptr};
+		bool output_supports_colour{false};
+
+		const char *get_colour(const std::string &str);
 
 	public:
+		// VT100 terminal color escape sequences
+		static constexpr const char *COLOUR_NORMAL{""};
+		static constexpr const char *COLOUR_RESET{"\033[m"};
+		static constexpr const char *COLOUR_BOLD_RED{"\033[1;31m"};
+		static constexpr const char *COLOUR_BOLD_BLUE{"\033[1;34m"};
+
 		Logger();
-		explicit Logger(std::string _prefix);
-		Logger(std::string _prefix, const std::string &file_path);
+		explicit Logger(const std::string &_prefix);
+		Logger(const std::string &_prefix, const std::string &file_path);
 		void log(const std::string &str);
 		void log(const boost::format &str);
-		void program_output(const std::string &mesg);
+		void program_output(const std::string &str);
+		void force_colour_output(bool set);
 	};
 } // namespace buildsys
 

unit-test/CMakeLists.txt
@@ -23,3 +23,10 @@ target_include_directories(builddir_unittests PRIVATE ../)
 target_compile_features(builddir_unittests PRIVATE cxx_std_14)
 target_link_libraries(builddir_unittests PRIVATE Catch2::Catch2)
 add_test(NAME builddir_unittests COMMAND builddir_unittests)
+
+add_executable(logger_unittests logger_unittests.cpp ../logger.cpp ../filesystem.cpp)
+append_coverage_compiler_flags()
+target_include_directories(logger_unittests PRIVATE ../)
+target_compile_features(logger_unittests PRIVATE cxx_std_14)
+target_link_libraries(logger_unittests PRIVATE Catch2::Catch2)
+add_test(NAME logger_unittests COMMAND logger_unittests)

commit ea5515c90ba5c4c7973b0cae1ec081b4792c05e0

logger: Fix coloured message output
Currently the messages are only covered if the strings begin with
"error:" or "warning:". The expected behaviour is to colour the
message if these appear anywhere in the string.

fixes: commit 0f4ad29 ("Add unit tests for new Logger class")

Change-Id: Ic7a5f79125dbe5b29572e6a966b5268da0ca2464
Reviewed-by: Scott Parlane <scott.parlane@alliedtelesis.co.nz>
Signed-off-by: Matt Bennett <matt.bennett@alliedtelesis.co.nz>

logger.cpp
@@ -106,10 +106,10 @@ const char *Logger::get_colour(const std::string &str)
 	const std::string warning_str("warning:");
 
 	if(this->output_supports_colour) {
-		if(str.compare(0, error_str.size(), error_str) == 0) {
+		if(str.find(error_str) != std::string::npos) {
 			return Logger::COLOUR_BOLD_RED;
 		}
-		if(str.compare(0, warning_str.size(), warning_str) == 0) {
+		if(str.find(warning_str) != std::string::npos) {
 			return Logger::COLOUR_BOLD_BLUE;
 		}
 	}

unit-test/logger_unittests.cpp
@@ -98,7 +98,24 @@ TEST_CASE_METHOD(LoggerTestsFixture, "Test Logger program_output() with coloured
 
 	buffer.str("");
 
+	test_string = std::string("Random text at start && error: This is a test log message");
+	logger.program_output(test_string);
+
+	REQUIRE(buffer.str() == std::string(Logger::COLOUR_BOLD_RED) + test_string +
+	                            std::string(Logger::COLOUR_RESET) + "\n");
+
+	buffer.str("");
+
 	test_string = std::string("warning: This is a test log message");
+	logger.program_output(test_string);
+
+	REQUIRE(buffer.str() == std::string(Logger::COLOUR_BOLD_BLUE) + test_string +
+	                            std::string(Logger::COLOUR_RESET) + "\n");
+
+	buffer.str("");
+
+	test_string =
+	    std::string("Random text at start && warning: This is a test log message");
 	logger.program_output(test_string);
 
 	REQUIRE(buffer.str() == std::string(Logger::COLOUR_BOLD_BLUE) + test_string +